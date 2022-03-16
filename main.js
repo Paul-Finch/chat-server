f3eff8ac597edab6e2185801b4cadad19e3c52dc
Konrad Schewe <konrad.schewe@sap.com>
2022-03-16 09:24:54 +0100

    Server can accept connections and sends chat history

--- main.js
+++ main.js
@@ -6,15 +6,13 @@ const http = require("http");
 const history = [];
 const clients = [];
 
-var server = http.createServer(function (request, response) {
+const server = http.createServer(function (request, response) {
   // HTTP Server stays empty - just needed to create WebScocker server
 });
 server.listen(webSocketsServerPort, function () {
-  console.log(
-    new Date() + " Server is listening on port " + webSocketsServerPort
-  );
+  console.log(new Date() + " Server is listening on port " + webSocketsServerPort);
 });
-var wsServer = new webSocketServer({
+const wsServer = new webSocketServer({
   // WebSocket server is tied to a HTTP server. WebSocket
   // request is just an enhanced HTTP request. For more info
   // http://tools.ietf.org/html/rfc6455#page-6
@@ -23,9 +21,19 @@ var wsServer = new webSocketServer({
 
 // Client connects
 wsServer.on("request", function (request) {
+  const connection = request.accept(null, request.origin);
+  const index = clients.push(connection) - 1;
+  let userName = false;
+  console.log((new Date()) + ' Connection accepted.');
+  // Send chat history
+  if (history.length > 0) {
+    connection.sendUTF(JSON.stringify({ type: 'history', data: history} ));
+  }
+
   // Client sent some message
   connection.on("message", function (message) {
   });
+
   // Client disconnects
   connection.on("close", function (connection) {
   });
