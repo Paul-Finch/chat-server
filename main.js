86ecc9bafff0297a981984679f2d55e02447f085
Konrad Schewe <konrad.schewe@sap.com>
2022-03-16 13:04:32 +0100

    set name on first message

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const webSocketsServerPort = 8353;
 const webSocketServer = require("websocket").server;
 const http = require("http");
 
-const history = [];
+let history = [];
 const clients = [];
 
 const server = http.createServer(function (request, response) {
@@ -25,35 +25,44 @@ const wsServer = new webSocketServer({
 wsServer.on("request", function (request) {
   const connection = request.accept(null, request.origin);
   const index = clients.push(connection) - 1;
-  let userName = "Test";
+  let userName = false;
   console.log(new Date() + " Connection accepted.");
-  // Send chat history
-  if (history.length > 0) {
-    connection.sendUTF(JSON.stringify({ type: "history", data: history }));
-  }
 
   // Client sent some message
   connection.on("message", function (message) {
     if (message.type === "utf8") {
-      console.log(
-        new Date() +
-          " Received Message from " +
-          userName +
-          ": " +
-          message.utf8Data
-      );
-      // we want to keep history of all sent messages
-      const obj = {
-        time: new Date().getTime(),
-        text: message.utf8Data,
-        author: userName,
-      };
-      history.push(obj);
-      history = history.slice(-100);
-      // broadcast message to all connected clients
-      const json = JSON.stringify({ type: "message", data: obj });
-      for (var i = 0; i < clients.length; i++) {
-        clients[i].sendUTF(json);
+      if (userName === false) {
+        userName = message.utf8Data;
+        // Send chat history
+        if (history.length > 0) {
+          connection.sendUTF(JSON.stringify({ type: "history", data: history }));
+        }
+        console.log(
+          new Date() +
+            " User is known as: " +
+            userName
+        );
+      } else {
+        console.log(
+          new Date() +
+            " Received Message from " +
+            userName +
+            ": " +
+            message.utf8Data
+        );
+        // we want to keep history of all sent messages
+        const obj = {
+          time: new Date().getTime(),
+          text: message.utf8Data,
+          author: userName,
+        };
+        history.push(obj);
+        history = history.slice(-100);
+        // broadcast message to all connected clients
+        const json = JSON.stringify({ type: "message", data: obj });
+        for (var i = 0; i < clients.length; i++) {
+          clients[i].sendUTF(json);
+        }
       }
     }
   });
